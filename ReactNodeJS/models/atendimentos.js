cd786320a7fd218b27874570d575901618319f0e
Poliana Morandi da Costa <72170872+polianamorandi@users.noreply.github.com>
2021-07-07 21:10:40 -0400

    Add files via upload

--- ReactNodeJS/models/atendimentos.js
+++ ReactNodeJS/models/atendimentos.js
@@ -2,12 +2,12 @@ const moment = require('moment')
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
-    adiciona(atendimento, res){
+    adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
-        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS') // formatação da data com a lib moment
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
         
-        const dataEhValida = moment(data).isSameOrAfter(dataCriacao) // função do moment isSameOrAfter, retorna booleano dizendo se a data é igual ou dps do segundo paramento que é a dataCriacao
-        const clienteEhValido = atendimento.cliente.length >= 3
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
 
         const validacoes = [
             {
@@ -15,36 +15,89 @@ class Atendimento {
                 valido: dataEhValida,
                 mensagem: 'Data deve ser maior ou igual a data atual'
             },
-            
             {
                 nome: 'cliente',
-                clienteEhValido: clienteEhValido,
-                mensagem: 'Nome do cliente deve conter no minimo 3 caracteres'
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
             }
         ]
 
-        const erros = validacoes.filter(campo => !campo.valido ) // só retorno se nao for valido
+        const erros = validacoes.filter(campo => !campo.valido)
         const existemErros = erros.length
-        
-        if (existemErros){
+
+        if(existemErros) {
             res.status(400).json(erros)
-        }else{
+        } else {
             const atendimentoDatado = {...atendimento, dataCriacao, data}
-           
+
             const sql = 'INSERT INTO Atendimentos SET ?'
     
             conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-                if (erro){
+                if(erro) {
                     res.status(400).json(erro)
-                }else{
+                } else {
                     res.status(201).json(atendimento)
                 }
-            })   
-    
+            })
+        }
+       
+    }
 
+    lista(res){
+        const sql = 'SELECT * FROM Atendimentos'
+        
+        conexao.query(sql, (erro, resultados) =>{
+            if (erro){
+                res.status(400).json(erro)
+            }else{
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    buscaPorId(id, res){
+        const sql = `SELECT * FROM Atendimentos WHERE id=${id}`
+
+        conexao.query(sql, (erro, resultados) => {
+            const atendimento = resultados[0]
+            if (erro){
+                res.status(400).json(erro)
+            }else{
+                res.status(200).json(atendimento)
+            }
+        })
+    }
+
+    altera(id, valores, res){
+
+        if (valores.data){
+            valores.data =   moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
         }
 
+        const sql = 'UPDATE Atendimentos SET ? WHERE id=?'
+
+        conexao.query(sql, [valores, id], (erro, resultados) => {
+            if (erro){
+                res.status(400).json(erro)
+            }else{
+                res.status(200).json({...valores, id})
+            }
+        })
     }
+
+    deleta(id, res){
+        const sql = 'DELETE FROM Atendimentos WHERE id=?'
+
+        conexao.query(sql, id, (erro, resultados) => {
+            if (erro){
+                res.status(40).json(erro)
+            }else{
+                res.status(200).json({id})
+            }
+        })
+    }
+
+
 }
 
 module.exports = new Atendimento
